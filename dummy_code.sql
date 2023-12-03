d7415b12d5f47e55613d332d84a5215a56070d0e
Ariyes <99893132+SafaeHaj@users.noreply.github.com>
2023-12-03 20:04:15 +0100

    Update dummy_code.sql

--- dummy_code.sql
+++ dummy_code.sql
@@ -9,40 +9,50 @@ GRANT SELECT ON project2.Flight TO 'user';
 DELIMITER //
 CREATE PROCEDURE CreateReservationView(IN user_email VARCHAR(32))
 BEGIN
-	DECLARE @view_name AS VARCHAR(46)
-	DECLARE @sql_query AS VARCHAR(164)
-	SET @view_name = CONCAT(SUBSTRING_INDEX(user_email, @, 1), 'ReservationView');
-	SET @sql_query = CONCAT('CREATE VIEW', @view_name, 'AS
-		SELECT rid, rdate, confirmDate
-		FROM Reservation
-		JOIN User
-		ON Reservation.uemail = User.uemail
-		WHERE User.uemail = ', QUOTE(user_email), ';',
-		GRANT INSERT, UPDATE, DELETE ON project2., @view_name, to 'user';);
-	PREPARE stmt FROM @sql_query;
-	EXECUTE stmt;
-	DEALLOCATE PREPARE stmt;
+    DECLARE view_name VARCHAR(46);
+    DECLARE sql_query VARCHAR(164);
+    DECLARE grant_query VARCHAR(86);
+
+    SET @view_name = CONCAT(SUBSTRING_INDEX(user_email, '@', 1), 'ReservationView');
+    SET @sql_query = CONCAT('CREATE VIEW ', @view_name, ' AS
+        SELECT rid, rdate, confirmDate
+        FROM Reservation
+        JOIN User
+        ON Reservation.uemail = User.uemail
+        WHERE User.uemail = ', QUOTE(user_email));
+
+    PREPARE stmt FROM sql_query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+    SET @grant_query = CONCAT('GRANT INSERT, UPDATE, DELETE ON project2.', @view_name, ' TO `user`');
+    PREPARE stmt FROM @grant_query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
 END //
-DELIMITER;
+DELIMITER ;
+
 
 DELIMITER //
 CREATE PROCEDURE CreatePassengerView(IN user_email VARCHAR(32))
 BEGIN
-	DECLARE @view_name AS VARCHAR(46)
-	DECLARE @sql_query AS VARCHAR(164)
+	DECLARE view_name VARCHAR(46);
+        DECLARE sql_query VARCHAR(164);
+        DECLARE grant_query VARCHAR(86);
 	SET @view_name = CONCAT(SUBSTRING_INDEX(user_email, @, 1), 'PassengerView');
 	SET @sql_query = CONCAT('CREATE VIEW', @view_name, 'AS 
-		CREATE VIEW PassengerView AS
-	SELECT passportID, cin, pfirstName AS firstName, plastName AS lastName, phoneNumber, 	pbirthDate AS birthDate
+	CREATE VIEW PassengerView AS
+	SELECT passportID, cin, pfirstName AS firstName, plastName AS lastName, phoneNumber, pbirthDate AS birthDate
 	FROM Passenger
 	JOIN Ticket ON Ticket.passportID = Passenger.passportID
 	JOIN Reservation ON Reservation.rid = Ticket.rid
 	JOIN User ON User.uemail = Reservation.uemail
-	WHERE User.uemail = ', QUOTE(user_email), ';',
-	GRANT INSERT, UPDATE, DELETE ON project2., @view_name, to 'user';
-	);
+	WHERE User.uemail = ', QUOTE(user_email), ';');
 	PREPARE stmt FROM @sql_query;
 	EXECUTE stmt;
 	DEALLOCATE PREPARE stmt;
+	SET @grant_query = CONCAT('GRANT INSERT, UPDATE, DELETE ON project2.', @view_name, ' TO `user`');
+   	PREPARE stmt FROM @grant_query;
+   	EXECUTE stmt;
+    	DEALLOCATE PREPARE stmt;
 END //
 DELIMITER;
