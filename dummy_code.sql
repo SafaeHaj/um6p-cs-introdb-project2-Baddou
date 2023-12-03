b266d41b9a3bd94e071fbf97da0d23d17dd4dc9f
Ariyes <99893132+SafaeHaj@users.noreply.github.com>
2023-12-03 19:53:40 +0100

    Update dummy_code.sql

--- dummy_code.sql
+++ dummy_code.sql
@@ -9,13 +9,15 @@ GRANT SELECT ON project2.Flight TO 'user';
 DELIMITER $$
 CREATE PROCEDURE CreateReservationView(IN user_email VARCHAR(32))
 BEGIN
+	DECLARE @view_name AS VARCHAR(46)
+	DECLARE @sql_query AS VARCHAR(164)
 	SET @view_name = CONCAT(SUBSTRING_INDEX(user_email, @, 1), 'ReservationView');
-	SET @sql_query = CONCAT('CREATE VIEW', @view_name, 'AS',
+	SET @sql_query = CONCAT('CREATE VIEW', @view_name, 'AS
 		SELECT rid, rdate, confirmDate
 		FROM Reservation
 		JOIN User
 		ON Reservation.uemail = User.uemail
-		WHERE User.uemail = , QUOTE(user_email), ';',
+		WHERE User.uemail = ', QUOTE(user_email), ';',
 		GRANT INSERT, UPDATE, DELETE ON project2., @view_name, to 'user';);
 	PREPARE stmt FROM @sql_query;
 	EXECUTE stmt;
@@ -26,15 +28,17 @@ DELIMITER;
 DELIMITER $$
 CREATE PROCEDURE CreatePassengerView(IN user_email VARCHAR(32))
 BEGIN
+	DECLARE @view_name AS VARCHAR(46)
+	DECLARE @sql_query AS VARCHAR(164)
 	SET @view_name = CONCAT(SUBSTRING_INDEX(user_email, @, 1), 'PassengerView');
-	SET @sql_query = CONCAT('CREATE VIEW', @view_name, 'AS',
+	SET @sql_query = CONCAT('CREATE VIEW', @view_name, 'AS 
 		CREATE VIEW PassengerView AS
 	SELECT passportID, cin, pfirstName AS firstName, plastName AS lastName, phoneNumber, 	pbirthDate AS birthDate
 	FROM Passenger
 	JOIN Ticket ON Ticket.passportID = Passenger.passportID
 	JOIN Reservation ON Reservation.rid = Ticket.rid
 	JOIN User ON User.uemail = Reservation.uemail
-	WHERE User.uemail = , QUOTE(user_email), ';',
+	WHERE User.uemail = ', QUOTE(user_email), ';',
 	GRANT INSERT, UPDATE, DELETE ON project2., @view_name, to 'user';
 	);
 	PREPARE stmt FROM @sql_query;
