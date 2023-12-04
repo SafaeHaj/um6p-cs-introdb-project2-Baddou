b9f2d8928ebb926f243bc9235399b17c2aa29875
Ariyes <99893132+SafaeHaj@users.noreply.github.com>
2023-12-04 22:44:37 +0100

    Update dummy_code.sql

--- dummy_code.sql
+++ dummy_code.sql
@@ -1,37 +1,16 @@
 CREATE ROLE 'user', 'airline', 'admin';
 
-GRANT ALL ON project2.Flight  TO 'airline';
-GRANT ALL ON project2.Airplane  TO 'airline';
 GRANT SELECT ON project2.Checks TO 'admin';
 GRANT ALL ON project2.Reservation TO 'admin';
 GRANT SELECT ON project2.Flight TO 'user';
-/*
-
-Problem with determining specifically airline users out of mysql.user table- TO DISCUSS
 
 DELIMITER //
-CREATE TRIGGER on_airline_delete BEFORE DELETE ON mysql.user
-FOR EACH ROW
+CREATE PROCEDURE on_user_add(IN user_email VARCHAR(32), IN user_password VARCHAR(64))
 BEGIN
-    IF 
-	DELETE FROM Airplane
-	WHERE Airplane.airline = OLD;
-	REVOKE 'airline' FROM OLD;
-	DROP OLD;
-    END IF;
-END;
-//
-*/
-
-DELIMITER //
-CREATE PROCEDURE on_user_add(IN user_email VARCHAR(32))
-BEGIN
-	DECLARE user_name VARCHAR(32);
-    IF NOT EXISTS (SELECT 1 FROM project2.User_ WHERE user.uemail = user_email) THEN
+    DECLARE user_name VARCHAR(32);
     SET user_name = SUBSTRING_INDEX(user_email, '@', 1);
-	CREATE USER user_name;
-	GRANT 'user' TO user_name;
-    END IF;
+    CREATE USER user_name IDENTIFIED BY user_password;
+    GRANT 'user' TO user_name;
 END;
 //
 
@@ -39,7 +18,7 @@ DELIMITER //
 CREATE TRIGGER trig_on_user_add AFTER INSERT ON User_
 FOR EACH ROW
 BEGIN 
-    CALL on_user_add(NEW.uemail);
+    CALL on_user_add(NEW.uemail, NEW.passwordHash);
 END;
 //
 
@@ -54,12 +33,12 @@ BEGIN
     END IF;
 END;//
 
+DELIMITER //
 CREATE TRIGGER trig_on_user_delete BEFORE DELETE ON User_
 FOR EACH ROW 
 BEGIN
     CALL on_user_delete(OLD.uemail);
-END;
-
+END;//
 
 DELIMITER //
 CREATE PROCEDURE CreateReservationView(IN user_email VARCHAR(32))
@@ -98,7 +77,6 @@ BEGIN
 	
 	SET @view_name = CONCAT(SUBSTRING_INDEX(user_email, '@', 1), 'PassengerView');
 	SET @create_user_view = CONCAT('CREATE VIEW', @view_name, 'AS 
-		CREATE VIEW PassengerView AS
 		SELECT passportID, cin, pfirstName AS firstName, plastName AS lastName, phoneNumber, pbirthDate AS birthDate
 		FROM Passenger JOIN Ticket ON Ticket.passportID = Passenger.passportID
 		JOIN Reservation ON Reservation.rid = Ticket.rid
@@ -111,3 +89,80 @@ BEGIN
     	DEALLOCATE PREPARE stmt;
 END; 
 // 
+
+DELIMITER //
+CREATE FUNCTION acronymize(name VARCHAR(64)) RETURNS VARCHAR(32)
+BEGIN
+    DECLARE acronomized VARCHAR(32) DEFAULT '';
+    DECLARE i INT DEFAULT 1;
+
+    SET name = TRIM(name);
+
+    WHILE i <= LENGTH(name) - LENGTH(REPLACE(name, ' ', '')) + 1 DO
+        SET acronomized = CONCAT(acronomized, UPPER(SUBSTRING_INDEX(SUBSTRING_INDEX(name, ' ', i), ' ', -1)));
+        SET i = i + 1;
+    END WHILE;
+
+    RETURN acronomized;
+END; //
+	
+DELIMITER //
+CREATE PROCEDURE CreateAirline(IN airline VARCHAR(64)) -- to call in loop in order to generate airline accounts
+BEGIN
+   DECLARE airline_acronym VARCHAR(32);
+   SET airline_acronym = acronomize(airline);
+   IF NOT EXISTS (SELECT 1 FROM mysql.user WHERE user = airline_acronym) 
+        CREATE USER airline_acronym;
+        GRANT 'airline' TO airline_acronym;
+   END IF;
+END; 
+// 
+
+DELIMITER //
+CREATE PROCEDURE CreateAirplaneView(IN airline VARCHAR(64))
+BEGIN
+	DECLARE airline_acronym VARCHAR(32);
+	SET airline_acronym = acronomize(airline);
+	IF NOT EXISTS (SELECT 1 FROM mysql.user WHERE user = airline_acronym) THEN
+	    SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'The airline you entered does not exist';
+	END IF;
+	
+	SET @view_name = CONCAT(airline_acronym, 'AirplaneView');
+	SET @create_view = CONCAT('CREATE VIEW', @view_name, 'AS 
+		SELECT *
+		FROM Airplane
+		WHERE Airplane.airline = ', airline, ';
+		GRANT SELECT, INSERT, DELETE ON project2.', @view_name, ' TO', airline_acronym,';');
+
+   	PREPARE stmt FROM @create_view;
+   	EXECUTE stmt;
+    	DEALLOCATE PREPARE stmt;
+END; 
+//
+
+DELIMITER //
+CREATE PROCEDURE CreateAirplaneModelView(IN airline VARCHAR(64))
+BEGIN
+	DECLARE airline_acronym VARCHAR(32);
+	SET airline_acronym = acronomize(airline);
+	IF NOT EXISTS (SELECT 1 FROM mysql.user WHERE user = airline_acronym) THEN
+	    SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'The airline you entered does not exist';
+	END IF;
+	
+	SET @view_name = CONCAT(airline_acronym, 'AirplaneModelView');
+	SET @create_view = CONCAT('CREATE VIEW', @view_name, 'AS 
+		SELECT *
+		FROM AirplaneModel
+		JOIN Airplane ON Airplane.model = AirplaneModel.model
+		WHERE AirplaneModel.airline = ', airline, ';
+		GRANT SELECT, INSERT, DELETE ON project2.', @view_name, ' TO', airline_acronym,';');
+
+   	PREPARE stmt FROM @create_view;
+   	EXECUTE stmt;
+    	DEALLOCATE PREPARE stmt;
+END; 
+//
+
+
