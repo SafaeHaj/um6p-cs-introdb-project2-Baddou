0bb2f9227203c9c4e9b23f2d2ddf1e7cc5f02000
Ariyes <99893132+SafaeHaj@users.noreply.github.com>
2023-12-05 14:30:41 +0100

    Update um6p-cs-introdb-project2-dcl.sql

--- um6p-cs-introdb-project2-dcl.sql
+++ um6p-cs-introdb-project2-dcl.sql
@@ -8,64 +8,40 @@ GRANT SELECT ON project2.Flight TO 'user';
 -----User account creation on Project2.User_ add-----
 -----------------------------------------------------
 DELIMITER //
-CREATE PROCEDURE on_user_add(IN user_email VARCHAR(32))
+CREATE PROCEDURE on_user_add(IN user_email VARCHAR(32), IN password_user VARCHAR(64))
 BEGIN
-    DECLARE user_name VARCHAR(32);
-    SET user_name = SUBSTRING_INDEX(user_email, '@', 1);
-    CREATE USER user_name;
-    GRANT 'user' TO user_name;
+    SET @create_user_query = CONCAT('CREATE USER ', QUOTE(user_email), '@\'%\' IDENTIFIED BY ', QUOTE(password_user), ';
+	GRANT ''user'' TO ', QUOTE(user_email));
+    PREPARE create_user_stmt FROM @create_user_query;
+    EXECUTE create_user_stmt;
+    DEALLOCATE PREPARE create_user_stmt;
 END;
 //
-
-DELIMITER //
-CREATE TRIGGER trig_on_user_add AFTER INSERT ON User_
-FOR EACH ROW
-BEGIN 
-    CALL on_user_add(NEW.uemail);
-END;
-//
-
 -----------------------------------------------------
 -----User account deletion on Project2.User_ delete--
 -----------------------------------------------------
 DELIMITER //
 CREATE PROCEDURE on_user_delete(IN user_email VARCHAR(32))
 BEGIN
-	DECLARE user_name VARCHAR(32);
-    IF EXISTS (SELECT 1 FROM project2.User_ WHERE user = user_name) THEN
-		SET user_name = SUBSTRING_INDEX(user_email, '@', 1);
-		REVOKE 'user' FROM user_name;
-		DROP USER user_name;
-    END IF;
-END;//
-
-DELIMITER //
-CREATE TRIGGER trig_on_user_delete BEFORE DELETE ON User_
-FOR EACH ROW 
-BEGIN
-    CALL on_user_delete(OLD.uemail);
-END;//
-
+    SET @revok_delete_query = CONCAT('REVOKE ''user'' FROM ', QUOTE(user_email), ';DROP USER ', QUOTE(user_email));
+    PREPARE revoke_stmt FROM @revoke_delete_query;
+    EXECUTE revoke_stmt;
+    DEALLOCATE PREPARE revoke_stmt;
+END;
+//
 -----------------------------------------------------
 -----Grant row access on user's reservation----------
 -----------------------------------------------------
 DELIMITER //
-CREATE PROCEDURE CreateReservationView(IN user_email VARCHAR(32))
+CREATE PROCEDURE create_reservation_view(IN user_email VARCHAR(32))
 BEGIN
-    DECLARE user_name VARCHAR(32);
-    SET user_name = SUBSTRING_INDEX(user_email, '@', 1);
-    IF NOT EXISTS (SELECT 1 FROM mysql.user WHERE user = user_name) THEN
-	SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'The user you entered does not exist';
-    END IF;
-
-    SET @view_name = CONCAT(SUBSTRING_INDEX(user_email, '@', 1), 'ReservationView');
+    SET @view_name = CONCAT(user_email, 'ReservationView');
     SET @create_user_view = CONCAT('CREATE VIEW ', @view_name, ' AS
-        SELECT rid, rdate, confirmDate
+        SELECT rid,
         FROM Reservation JOIN User
         ON Reservation.uemail = User.uemail
         WHERE User.uemail = ', user_email,
-	"GRANT SELECT, INSERT, UPDATE, DELETE ON project2.", @view_name, " TO ", user_name, ';');
+	"GRANT SELECT, INSERT, UPDATE, DELETE ON project2.", @view_name, " TO ",user_email, ';');
 
     PREPARE stmt FROM @create_user_view;
     EXECUTE stmt;
@@ -77,23 +53,16 @@ DELIMITER ;
 -----Grant row access on user's registered passengers----------
 ---------------------------------------------------------------
 DELIMITER //
-CREATE PROCEDURE CreatePassengerView(IN user_email VARCHAR(32))
+CREATE PROCEDURE create_passenger_view(IN user_email VARCHAR(32))
 BEGIN
-	DECLARE user_name VARCHAR(32);
-	SET user_name = SUBSTRING_INDEX(user_email, '@', 1);
-	IF NOT EXISTS (SELECT 1 FROM mysql.user WHERE user = user_name) THEN
-	    SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'The user you entered does not exist';
-	END IF;
-	
-	SET @view_name = CONCAT(SUBSTRING_INDEX(user_email, '@', 1), 'PassengerView');
-	SET @create_user_view = CONCAT('CREATE VIEW', @view_name, 'AS 
-		SELECT passportID, cin, pfirstName AS firstName, plastName AS lastName, phoneNumber, pbirthDate AS birthDate
-		FROM Passenger JOIN Ticket ON Ticket.passportID = Passenger.passportID
-		JOIN Reservation ON Reservation.rid = Ticket.rid
-		JOIN User ON User.uemail = Reservation.uemail
-		WHERE User.uemail = ', user_email, ';
-		GRANT SELECT, INSERT, UPDATE, DELETE ON project2.', @view_name, ' TO', user_name,';');
+   SET @view_name = CONCAT(SUBSTRING_INDEX(user_email, '@', 1), 'PassengerView');
+   SET @create_user_view = CONCAT('CREATE VIEW', @view_name, 'AS 
+	SELECT passportID, cin, pfirstName AS firstName, plastName AS lastName, phoneNumber, pbirthDate AS birthDate
+	FROM Passenger JOIN Ticket ON Ticket.passportID = Passenger.passportID
+	JOIN Reservation ON Reservation.rid = Ticket.rid
+	JOIN User ON User.uemail = Reservation.uemail
+	WHERE User.uemail = ', user_email, ';
+	GRANT SELECT, INSERT, UPDATE, DELETE ON project2.', @view_name, ' TO', user_name,';');
 
    	PREPARE stmt FROM @create_user_view;
    	EXECUTE stmt;
@@ -113,9 +82,7 @@ CREATE FUNCTION acronymize(name VARCHAR(64)) RETURNS VARCHAR(32) DETERMINISTIC
 BEGIN
     DECLARE acronomized VARCHAR(32) DEFAULT '';
     DECLARE i INT DEFAULT 1;
-
     SET name = TRIM(name);
-
     WHILE i <= LENGTH(name) - LENGTH(REPLACE(name, ' ', '')) + 1 DO
         SET acronomized = CONCAT(acronomized, UPPER(SUBSTRING_INDEX(SUBSTRING_INDEX(name, ' ', i), ' ', -1)));
         SET i = i + 1;
@@ -128,15 +95,16 @@ END;//
 -----Airline user account creation----
 --------------------------------------
 DELIMITER //
-CREATE PROCEDURE CreateAirline(IN airline VARCHAR(64))
+CREATE PROCEDURE create_airline(IN airline VARCHAR(64), IN airline_password VARCHAR(64))
 BEGIN
-   DECLARE airline_acronym VARCHAR(32);
-   SET airline_acronym = acronomize(airline);
-   
-   IF NOT EXISTS (SELECT 1 FROM mysql.user WHERE user = airline_acronym) THEN
-        CREATE USER airline_acronym;
-        GRANT 'airline' TO airline_acronym;
-   END IF;
+   DECLARE acronym_airline VARCHAR(32);
+   SET acronym_airline = acronymize(airline);
+   SET @create_airline_query = CONCAT('CREATE USER ', QUOTE(acronym_airline), '@\'%\' IDENTIFIED BY ', QUOTE(airline_password), ';
+	GRANT ''airline'' TO ', QUOTE(acronym_airline));
+
+   PREPARE create_airline_stmt FROM @create_airline_query;
+   EXECUTE create_airline_stmt;
+   DEALLOCATE PREPARE create_airline_stmt;
 END; 
 //
 
@@ -147,12 +115,6 @@ DELIMITER //
 CREATE PROCEDURE CreateAirplaneView(IN airline VARCHAR(64))
 BEGIN
 	DECLARE airline_acronym VARCHAR(32);
-	SET airline_acronym = acronomize(airline);
-	IF NOT EXISTS (SELECT 1 FROM mysql.user WHERE user = airline_acronym) THEN
-	    SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'The airline you entered does not exist';
-	END IF;
-	
 	SET @view_name = CONCAT(airline_acronym, 'AirplaneView');
 	SET @create_view = CONCAT('CREATE VIEW', @view_name, 'AS 
 		SELECT *
@@ -171,14 +133,7 @@ END;
 ------------------------------------------------------
 DELIMITER //
 CREATE PROCEDURE CreateAirplaneModelView(IN airline VARCHAR(64))
-BEGIN
-	DECLARE airline_acronym VARCHAR(32);
-	SET airline_acronym = acronomize(airline);
-	IF NOT EXISTS (SELECT 1 FROM mysql.user WHERE user = airline_acronym) THEN
-	    SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'The airline you entered does not exist';
-	END IF;
-	
+BEGIN	
 	SET @view_name = CONCAT(airline_acronym, 'AirplaneModelView');
 	SET @create_view = CONCAT('CREATE VIEW', @view_name, 'AS 
 		SELECT *
