9f9cf1bbbd999244b83132f797937b9df7f4645b
Ariyes <99893132+SafaeHaj@users.noreply.github.com>
2024-01-04 21:34:18 +0100

    Update PROCEDURES.sql

--- sql/PROCEDURES.sql
+++ sql/PROCEDURES.sql
@@ -1,3 +1,47 @@
+DELIMITER //
+
+CREATE PROCEDURE AddTicketToReservation(
+	IN ticketId VARCHAR(20),
+    IN passportID VARCHAR(20),
+    IN rid1 VARCHAR(20),
+    IN fid VARCHAR(20),
+    IN fctype VARCHAR(20),
+    IN ticketClass VARCHAR(20)
+)
+
+BEGIN 
+
+	DECLARE reservationExists INT;
+    DECLARE tidExists INT;
+    
+    SELECT COUNT(*) INTO reservationExists
+    FROM Reservation
+    WHERE rid = rid1;
+    
+    SELECT MAX(seatNumber) +1 INTO seatnumber
+    FROM Ticket
+    WHERE fid=fid and class=ticketClass;
+    
+    SELECT COUNT(*) INTO tidExists
+    FROM Ticket
+    WHERE tid = ticketId;
+    START TRANSACTION;
+    
+    IF reservationExists > 0 AND tidExists=0 THEN 
+		INSERT INTO Ticket (tid, seatNumber, passportID, rid, fid, fctype, class) VALUES (ticketId, seatnumber, passportId, rid1, fid, fctype, ticketClass);  
+	else
+		ROLLBACK;
+        SIGNAL SQLSTATE '45000'
+			SET MESSAGE_TEXT = ' Ticket already exists in a reservation';
+	END IF;
+    
+    COMMIT;
+    
+END //
+
+
+
+
 USE project2;
 drop procedure if exists CalculateTicketPrice;
 DELIMITER //
